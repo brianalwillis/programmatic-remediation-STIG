d9f52dbe23b42b3d6d206ca9b9a1a9f650998e0a
Briana <brianalw@yahoo.com>
2025-07-23 03:14:06 -0400

    Create WN10-SO-000265.ps1

---
+++ STIG/WN10-SO-000265.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures the User Account Control elevates UIAccess applications that are installed in secure locations.
+
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 6-30-2025
+    Last Modified   : 6-30-2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000265 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Define the registry path and value
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$valueName = "EnableSecureUIAPaths"
+$valueData = 1
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the DWORD value
+New-ItemProperty -Path $regPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+# Optional: Confirm the value
+$setValue = Get-ItemProperty -Path $regPath -Name $valueName
+Write-Output "$valueName set to: $($setValue.$valueName)"
