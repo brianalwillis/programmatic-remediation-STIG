3706ea1a8002cbe341d673e59fe5141d9622409b
Briana <brianalw@yahoo.com>
2025-07-23 05:17:50 -0400

    Update WN10-CC-000325.ps1

--- STIG/WN10-CC-000325.ps1
+++ STIG/WN10-CC-000325.ps1
@@ -1,1 +1,45 @@
+<#
+.SYNOPSIS
+    Automatically signing in the last interactive user after a system-initiated restart must be disabled.
 
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 2025-07-23
+    Last Modified   : 2025-07-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000325
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000325).ps1 
+#>
+
+# Run this script as Administrator
+
+# Define registry path and values
+$regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+$regName = 'DisableAutomaticRestartSignOn'
+$desiredValue = 1
+
+# Create registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required value
+Set-ItemProperty -Path $regPath -Name $regName -Value $desiredValue -Type DWord
+
+# Confirm the change
+$currentValue = Get-ItemPropertyValue -Path $regPath -Name $regName
+Write-Host "DisableAutomaticRestartSignOn is set to: $currentValue (1 = Disabled)"
