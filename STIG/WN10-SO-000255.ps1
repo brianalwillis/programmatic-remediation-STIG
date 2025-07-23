950345d01ead9c05e8fc66fa013b27d4802a1adb
Briana <brianalw@yahoo.com>
2025-07-23 05:20:29 -0400

    Update WN10-SO-000255.ps1

--- STIG/WN10-SO-000255.ps1
+++ STIG/WN10-SO-000255.ps1
@@ -1,1 +1,45 @@
+<#
+.SYNOPSIS
+    User Account Control must automatically deny elevation requests for standard users.
 
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 2025-07-23
+    Last Modified   : 2025-07-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000255
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000255).ps1 
+#>
+
+# Run this script as Administrator
+
+# Define registry path and values
+$regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+$regName = 'ConsentPromptBehaviorUser'
+$desiredValue = 0
+
+# Create registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required value
+Set-ItemProperty -Path $regPath -Name $regName -Value $desiredValue -Type DWord
+
+# Confirm the change
+$currentValue = Get-ItemPropertyValue -Path $regPath -Name $regName
+Write-Host "'ConsentPromptBehaviorUser' is set to: $currentValue (0 = Elevation requests automatically denied for standard users)"
