653ee8513e4ed60dea96b91dca08b545e26ed05d
Briana <brianalw@yahoo.com>
2025-07-23 05:18:45 -0400

    Update WN10-CC-000370.ps1

--- STIG/WN10-CC-000370.ps1
+++ STIG/WN10-CC-000370.ps1
@@ -1,1 +1,45 @@
+<#
+.SYNOPSIS
+    The convenience PIN for Windows 10 must be disabled.
 
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 2025-07-23
+    Last Modified   : 2025-07-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000370
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000370).ps1 
+#>
+
+# Run this script as Administrator
+
+# Define registry path and values
+$regPath = 'HKLM:\Software\Policies\Microsoft\Windows\System'
+$regName = 'AllowDomainPINLogon'
+$desiredValue = 0
+
+# Create registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the required value
+Set-ItemProperty -Path $regPath -Name $regName -Value $desiredValue -Type DWord
+
+# Confirm the change
+$currentValue = Get-ItemPropertyValue -Path $regPath -Name $regName
+Write-Host "AllowDomainPINLogon is set to: $currentValue (0 = Disabled)"
