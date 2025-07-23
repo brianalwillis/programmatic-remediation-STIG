1800f9f34c626bdd2b7e0a0808ad5b4b862ca556
Briana <brianalw@yahoo.com>
2025-07-23 03:13:21 -0400

    Create WN10-SO-000030.ps1

---
+++ STIG/WN10-SO-000030.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that an audit policy using subcategories is enabled.
+
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 6-30-2025
+    Last Modified   : 6-30-2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000030
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Define the registry path and value
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$valueName = "SCENoApplyLegacyAuditPolicy"
+$valueData = 1
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the DWORD value
+New-ItemProperty -Path $regPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+# Optional: Confirm the value is set
+$setValue = Get-ItemProperty -Path $regPath -Name $valueName
+Write-Output "$valueName set to: $($setValue.$valueName)"
