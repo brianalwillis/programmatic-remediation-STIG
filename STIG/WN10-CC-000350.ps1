18e1928ce7e68f8e003cd06beec7b6b30673aee5
Briana <brianalw@yahoo.com>
2025-08-16 14:56:15 -0400

    Create WN10-CC-000350.ps1

---
+++ STIG/WN10-CC-000350.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    The Windows Remote Management (WinRM) service must not allow unencrypted traffic.
+
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 2025-08-16
+    Last Modified   : 2025-08-16
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000350
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000350).ps1 
+#>
+
+# Run this script as Administrator
+
+# Registry path and value
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service"
+$ValueName = "AllowUnencryptedTraffic"
+$DesiredValue = 0
+
+# Ensure the registry key exists
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Get current value
+$currentValue = Get-ItemProperty -Path $RegPath -Name $ValueName -ErrorAction SilentlyContinue
+
+# Set value if missing or incorrect
+if ($null -eq $currentValue.$ValueName -or $currentValue.$ValueName -ne $DesiredValue) {
+    Set-ItemProperty -Path $RegPath -Name $ValueName -Value $DesiredValue -Type DWord
+    Write-Output "Registry value $ValueName set to $DesiredValue successfully"
+} else {
+    Write-Output "$ValueName is already configured correctly"
+}
