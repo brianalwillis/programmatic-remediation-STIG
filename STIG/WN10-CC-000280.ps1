4ad425354b02a0703bfe8a402f35cdee5f16bd4e
Briana <brianalw@yahoo.com>
2025-08-16 14:50:24 -0400

    Create WN10-CC-000280.ps1

---
+++ STIG/WN10-CC-000280.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    IPv6 source routing must be configured to the highest protection.
+
+.NOTES
+    Author          : Briana Willis
+    LinkedIn        : linkedin.com/in/brianalwillis
+    GitHub          : github.com/brianalwillis
+    Date Created    : 2025-08-16
+    Last Modified   : 2025-08-16
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000280
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       :
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000280).ps1 
+#>
+
+# Run this script as Administrator
+
+# Ensure fPromptForPassword is set to 1
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$ValueName = "fPromptForPassword"
+$DesiredValue = 1
+
+# Check if the registry key exists; create if missing
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force
+}
+
+# Get current value
+$currentValue = Get-ItemProperty -Path $RegPath -Name $ValueName -ErrorAction SilentlyContinue
+
+# If value is missing or not equal to desired, set it
+if ($null -eq $currentValue.$ValueName -or $currentValue.$ValueName -ne $DesiredValue) {
+    Set-ItemProperty -Path $RegPath -Name $ValueName -Value $DesiredValue -Type DWord
+    Write-Output "Registry value fPromptForPassword set to $DesiredValue"
+} else {
+    Write-Output "Registry value fPromptForPassword is already configured correctly"
+}
